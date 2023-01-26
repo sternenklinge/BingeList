bd446010fe0ce743aedba6c3803aa7a70a18702f
DeadDragonEye <duhuph@gmail.com>
2023-01-26 12:44:51 +0100

    configure settings.py for api

--- BingeList/settings.py
+++ BingeList/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #user apps
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    #user apps
+    'api'
 ]
 
 ROOT_URLCONF = 'BingeList.urls'
