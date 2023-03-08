2a54ee07998e29496326153979129a0813c75384
Mangosaft23 <elianbeyer@protonmail.com>
2023-03-08 22:39:38 +0100

    jscript modals

--- api/script.js
+++ api/script.js
@@ -1,1 +1,49 @@
-//const openModalButtons
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const addTargetButtons = document.querySelectorAll('[data-add-target]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+addTargetButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const target = document.querySelector(button.dataset.addTarget)
+        addToWatchlist(target)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+function addToWatchlist(target) {
+    if (target == null) return
+    
+}
